62a06b29ad86478a06217a6980caea01ee14717a
LoveChuotXun <doanvanhuy268@gmail.com>
2021-09-12 23:15:54 +0700

    Updates

---
+++ static/js/17.b701c545.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpscm-web-admin"]=this["webpackJsonpscm-web-admin"]||[]).push([[17],{76:function(e,n,s){"use strict";s.r(n),s.d(n,"default",(function(){return i}));var t=s(56),c=s(4);function i(){return Object(c.jsx)("div",{className:"h-auto pb-52",children:Object(c.jsx)(t.e,{})})}}}]);
+//# sourceMappingURL=17.b701c545.chunk.js.map
